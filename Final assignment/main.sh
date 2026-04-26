4daf98903679032c6eba207eefc98faeb36c256b
Wilbur <w.p.v.lierop@student.tue.nl>
2026-04-26 15:53:02 +0200

    again edit main.sh# Please enter the commit message for your changes. Lines starting

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -3,7 +3,7 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 16 \
-    --epochs 400 \
+    --epochs 250 \
     --lr 0.0001 \
     --num-workers 10 \
     --seed 42 \
